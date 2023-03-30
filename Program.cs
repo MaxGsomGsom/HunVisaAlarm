73da502214f1395054225225665498e3501d72e7
Maksim Kuzmin <max_kuzmin@outlook.com>
2023-03-30 14:03:39 +0300

    Updated to new version of web portal

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -11,41 +13,69 @@ namespace VisaAlarm
         static void Main(string[] args)
         {
             var paramLines = File.ReadAllLines("Params.txt");
-            var intervalInSec = int.Parse(paramLines[0]);
-            var login = paramLines[1];
+            string GetLine(int num) => paramLines[num].Split("#")[0].Trim();
+            var intervalInSec = int.Parse(GetLine(0));
+            var login = GetLine(1);
             var pass = paramLines[2];
+            var url = GetLine(3);
+            var visaCenter = GetLine(4);
+            var visaType = GetLine(5);
+            var visaSubType = GetLine(6);
+            var latestDate = DateTime.Parse(GetLine(7));
             var driver = new ChromeDriver();
+            
+            driver.Url = url;
+            Thread.Sleep(Sec(5));
+            driver.FindElement(By.XPath("//button[@id='onetrust-accept-btn-handler']")).Click();
+            Thread.Sleep(Sec(5));
+            driver.FindElement(By.XPath("//input[@formcontrolname='username']")).SendKeys(login);
+            Thread.Sleep(Sec(5));
+            driver.FindElement(By.XPath("//input[@formcontrolname='password']")).SendKeys(pass);
+            Thread.Sleep(Sec(5));
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//iframe[@title='reCAPTCHA']")));
+            driver.FindElement(By.XPath("//span[contains(@class, 'recaptcha-checkbox')]")).Click();
+            Thread.Sleep(Sec(5));
+            while (driver.FindElements(By.XPath("//span[contains(@class, 'recaptcha-checkbox-checked')]")).Count == 0)
+                Thread.Sleep(Sec(1));
+            driver.SwitchTo().ParentFrame();
+            driver.FindElement(By.XPath("//button[@mat-stroked-button]")).Click();
+            Thread.Sleep(Sec(10));
+                
+            driver.FindElements(By.XPath("//button[@mat-raised-button]"))[1].Click();
+            Thread.Sleep(Sec(10));
+            
+            Console.WriteLine("Initialization completed");
 
             while (true)
             {
                 Console.WriteLine("Last iteration time: " + DateTime.Now);
-                driver.Url = "https://visa.vfsglobal.com/rus/ru/hun/login";
-                Thread.Sleep(Sec(5));
-                driver.FindElement(By.XPath("//button[@id='onetrust-accept-btn-handler']")).Click();
-                Thread.Sleep(Sec(5));
-                driver.FindElement(By.XPath("//input[@formcontrolname='username']")).SendKeys(login);
-                Thread.Sleep(Sec(5));
-                driver.FindElement(By.XPath("//input[@formcontrolname='password']")).SendKeys(pass);
-                Thread.Sleep(Sec(5));
-                driver.FindElement(By.XPath("//button[@mat-raised-button]")).Click();
-                Thread.Sleep(Sec(10));
-                driver.FindElements(By.XPath("//button[@mat-raised-button]"))[1].Click();
-                Thread.Sleep(Sec(10));
                 driver.FindElement(By.XPath("//div[@id='mat-select-value-1']")).Click();
                 Thread.Sleep(Sec(5));
-                driver.FindElements(By.XPath("//span[@class='mat-option-text']"))[0].Click();
+                driver.FindElement(By.XPath($"//mat-option/*[contains(text(), '{visaCenter}')]")).Click();
+                Thread.Sleep(Sec(5));
+                driver.FindElement(By.XPath("//div[@id='mat-select-value-3']")).Click();
+                Thread.Sleep(Sec(5));
+                driver.FindElement(By.XPath($"//mat-option/*[contains(text(), '{visaType}')]")).Click();
                 Thread.Sleep(Sec(5));
                 driver.ExecuteScript("window.scrollTo(0, 99999)");
                 Thread.Sleep(Sec(5));
                 driver.FindElement(By.XPath("//div[@id='mat-select-value-5']")).Click();
-                Thread.Sleep(Sec(10));
-                driver.FindElements(By.XPath("//span[@class='mat-option-text']"))[4].Click();
+                Thread.Sleep(Sec(5));
+                driver.FindElement(By.XPath($"//mat-option/*[contains(text(), '{visaSubType}')]")).Click();
                 Thread.Sleep(Sec(10));
 
-                if (driver.FindElements(By.XPath("//div[@class='alert alert-info border-0 rounded-0']")).Count == 0)
+                var dateElement = driver.FindElements(By.XPath("//div[@class='alert alert-info border-0 rounded-0']"))
+                    .FirstOrDefault();
+                if (dateElement != null)
                 {
-                    Console.Beep(1000, Sec(10));
-                    Console.ReadLine();
+                    var dateString = Regex.Matches(dateElement.Text, "\\d{2}-\\d{2}-\\d{4}").FirstOrDefault()?.Value;
+                    Console.WriteLine("Found date: " + dateString);
+                    if (dateString != null && DateTime.Parse(dateString) < latestDate)
+                    {
+                        Console.WriteLine("Found correct date!");
+                        Console.Beep(1000, Sec(10));
+                        Console.ReadLine();
+                    }
                 }
 
                 Thread.Sleep(Sec(intervalInSec));
